54a95b832a4ce1928cb143859b7d6fd072685778
Ronnie <rbeggs10@gmail.com>
2020-06-30 21:54:18 -0700

    Add representation method along with finishing all methods in the Pokemon class

--- pokemon.py
+++ pokemon.py
@@ -1,57 +1,68 @@
 
 class Pokemon:
-    type_advantages = {'water':'grass', 'fire':'water', 'grass':'fire'}
+    #creates type dis/advantes for fire, water, and grass types
+    type_advantages = {'grass':'water', 'water':'fire', 'fire':'grass'}
+    type_disadvantages = {'water':'grass', 'fire':'water', 'grass':'fire'}
 
-    def __init__(self, name, level, type, current_health, status):
+    def __init__(self, name, level=5, type, status):
         self.name = name
         self.level = level
         self.type = type
-        self.max_health = level * 2
-        self.current_health = current_health
-        self.status = status
+        self.max_health = level * 4
+        self.current_health = level * 4
+        self.knocked_out = False
+        self.weakness = type_disadvantages.get(type)
+        self.advantage = type_advantages.get(type)
 
-    def attack(type, opponent_type):
-        poke_weakness = type_advantages.get(self.type)
-        damage = 10
-        if poke_weakness == opponent_type:
-            damage = damage / 2
-        elif poke_type == opponent_weakness:
-            damage = damage * 2
-        else:
-            damage = damage
-        return damage
+    def __repr__(self):
+        #printing a pokemon displays its name, type, level, and its hit points
+        return "{} type, level {} {} has {} hit points remaining.".format(self.type, self.level, self.name, self.current_health)
 
-    def lose_health():
-        max_health = 40
-        current_health = max_health
-        damage = attack(char_type, char_weakness, squirt_type, squirt_weakness)
-        current_health = current_health - damage
-        return current_health
+    def gain_health(self, amount):
+        #takes a health potion and applies the health to the pokemon
+        self.current_health += amount
+        print("{name} just used a potion and gained {potion} hit points!".format(name=self.name, potion=amount))
+        return self.current_health
 
-    def poke_status():
+    def lose_health(self, amount):
+        #takes an amount of damage and applies it to the pokemon
+        self.current_health -= amount
+        print("{name} just got hit and lost {damage} hit points!".format(name=self.name, damage=amount))
+        return self.current_health
+
+    def knock_out(self):
+        #if the pokemons health is less than or equal to zero, the pokemon is knocked out
         if self.current_health <= 0:
-            self.status = 0
+            self.knocked_out = True
             print("{} has fainted.".format(self.name))
 
-char_type = 'fire'
-bulb_type = 'grass'
-squirt_type = 'water'
-#the key is weak to its value
-type_advantages = {'water':'grass', 'fire':'water', 'grass':'fire'}
-char_weakness = type_advantages.get(char_type)
-bulb_weakness = type_advantages.get(bulb_type)
-squirt_weakness = type_advantages.get(squirt_type)
-#print(char_weakness)
+    def revive(self):
+        #checks to make sure that the pokemon is knocked out before reviving it
+        if self.current_health <= 0 and self.knocked_out == True:
+            self.knocked_out = False
+            self.current_health += (self.max_health / 2)
+            print("{name} was revived and has returned to the battle!".format(name=self.name))
 
-#instantiate char
-char = Pokemon('Charmander', 5, 'fire', 10, 1)
-
-
-#char_v_bulb = attack(char_type, char_weakness, bulb_type, bulb_weakness)
-#print(char_v_bulb)
-
-#char_v_squirt = attack(char_type, char_weakness, squirt_type, squirt_weakness)
-#print(char_v_squirt)
-
-#char_v_char = attack(char_type, char_weakness, char_type, char_weakness)
-#print(char_v_char)
+    def attack(self, opponent_poke):
+        #checks to see if the pokemon is able to attack the opponent
+        if self.knocked_out == True:
+            return "This pokemon has fainted and is unable to attack!"
+        else:
+            damage = (self.max_health / 3)
+            #determines damage if it's a not very effective matchup
+            if self.weakness == opponent_poke.type:
+                matchup_damage = damage / 2
+                opponent_poke.lose_health(matchup_damage)
+                print("It's Not Very Effective!")
+                print("{my_poke} attacked {oppo_poke} for {damage} damage.".format(my_poke=self.name, oppo_poke=opponent_poke.name, damage=matchup_damage))
+            #determines damage if it's a supereffective matchup
+            elif self.advantage == opponent_poke.type:
+                matchup_damage = damage * 2
+                opponent_poke.lose_health(matchup_damage)
+                print("It's Super Effective!")
+                print("{my_poke} attacked {oppo_poke} for {damage} damage.".format(my_poke=self.name, oppo_poke=opponent_poke.name, damage=matchup_damage))
+                #determines damage if it's a neutral matchup
+            else:
+                matchup_damage = damage
+                opponent_poke.lose_health(matchup_damage)
+                print("{my_poke} attacked {oppo_poke} for {damage} damage.".format(my_poke=self.name, oppo_poke=opponent_poke.name, damage=matchup_damage))
