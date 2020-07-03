ff133ad5c65db813bc3caaf0244a4195827f3799
Ronnie <rbeggs10@gmail.com>
2020-07-02 17:00:36 -0700

    Create a Trainer Class and with use potion, switch pokemon, and attack opponent methods

--- pokemon.py
+++ pokemon.py
@@ -21,16 +21,16 @@ class Pokemon:
     def gain_health(self, amount):
         #takes a health potion and applies the health to the pokemon
         self.current_health += amount
-        print("{name} just used a potion and gained {potion} hit points!".format(name=self.name, potion=amount))
+        print("{name} now has {health} health!".format(name=self.name, health=self.current_health))
         return self.current_health
 
     def lose_health(self, amount):
         #takes an amount of damage and applies it to the pokemon
         self.current_health -= amount
-        print("{name} just got hit and lost {damage} hit points!".format(name=self.name, damage=amount))
+        print("{name} was attacked and lost {damage} hit points! {name} now has {health} health.".format(name=self.name, damage=amount, health=self.current_health))
         return self.current_health
 
-    def knock_out(self):
+    def knocked_out(self):
         #if the pokemons health is less than or equal to zero, the pokemon is knocked out
         if self.current_health <= 0:
             self.knocked_out = True
@@ -61,8 +61,53 @@ class Pokemon:
                 opponent_poke.lose_health(matchup_damage)
                 print("It's Super Effective!")
                 print("{my_poke} attacked {oppo_poke} for {damage} damage.".format(my_poke=self.name, oppo_poke=opponent_poke.name, damage=matchup_damage))
-                #determines damage if it's a neutral matchup
+            #determines damage if it's a neutral matchup
             else:
                 matchup_damage = damage
                 opponent_poke.lose_health(matchup_damage)
                 print("{my_poke} attacked {oppo_poke} for {damage} damage.".format(my_poke=self.name, oppo_poke=opponent_poke.name, damage=matchup_damage))
+
+class Trainer:
+
+    def __init__(self, pokemon_list, potions, trainer_name):
+        self.pokemon_list = pokemon_list
+        self.potions = potions
+        self.trainer_name = trainer_name
+        self.current_pokemon = 0
+
+    def __repr__(self):
+        #lists off the trainers pokemon and shows which one is currently in battle
+        print("{trainer} has the following pokemon:".format(trainer=self.trainer_name))
+        for pokemon in pokemon_list:
+            print(pokemon)
+        return "{pokemon} is currently in battle.".format(pokemon=self.pokemon_list[self.current_pokemon].name)
+
+    def use_potion(self):
+        #makes sure that the trainer has enough potions before using them, then increases the health of the current pokemon
+        potion = self.max_health * .40
+        if self.potions > 0:
+            print("{trainer} used a potion on {pokemon}.".format(trainer=self.trainer_name, pokemon=self.current_pokemon))
+            self.current_pokemon.gain_health(potion)
+            self.potions -= 1
+        else:
+            print("You don't have any potions left!")
+
+    def attack_opposing_trainer(self, other_trainer):
+        #finds the current pokemon and its opponent, and attacks the opponent
+        opposing_pokemon = other_trainer.pokemon_list[other_trainer.current_pokemon]
+        my_pokemon = self.pokemon_list[self.current_pokemon]
+        my_pokemon.attack(opposing_pokemon)
+
+    def switch_active_pokemon(self, new_active):
+        #checks to make sure that the new_active value can be applied to the pokemon_list
+        if new_active < len(self.pokemon_list) and new_active >= 0:
+            #checks if the the pokemon is knocked out
+            if self.pokemon_list[new_active].knocked_out:
+                print("{pokemon} is knocked out! It's unable to enter the battle!".format(pokemon=self.pokemon_list[new_active].name))
+            #checks whether the pokemon is already in battle
+            elif self.current_pokemon == new_active:
+                print("This pokemon is already in battle!")
+            #switches in the new pokemon
+            else:
+                print("{new_active} has entered the battle!".format(new_active=self.current_pokemon[new_active].name))
+                self.current_pokemon = new_active
